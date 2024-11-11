5409cccc10fafca041474f6a1f997bdbc16b9052
Vasyl Dontsul <vgoatd@gmail.com>
2024-11-11 17:11:07 +0200

    feat: add origin log

--- controller/verifyEmail.js
+++ controller/verifyEmail.js
@@ -1,16 +1,18 @@
 export const verifyEmail = async (req, res) => {
+  const origin = req.headers.origin;
   const { email } = req.body;
 
-  console.log(req.body, "req.body");
   try {
     const emails = ["test@mail.com", "dontsul.v@gmail.com", "vgoatd@gmail.com"];
 
     if (!email) {
       throw new Error("email not found");
     }
+
     const isVerified = emails.includes(email) ? true : false;
 
-    console.log(isVerified, "isVerified result");
+    console.log(origin, "origin data");
+    // console.log(isVerified, "isVerified result");
     return res.json({ isVerified });
   } catch (error) {
     console.log("error");
